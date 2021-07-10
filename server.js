7fcd89831eae131e4881d5d0c79fffa80baabb87
ali-linux <mynameisali1999@gmail.com>
2021-07-10 22:15:30 +0300

    typescript

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 "use strict";
+// import axios from 'axios';
+// const url: string = 'https://jsonplaceholder.typicode.com/todos/1'
+// const num:Number = 1
+// const a = axios.get(url).then((r) => {
+//   console.log(r.data)
+//   console.log("<<NUMBER>>", num)
+// })
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const axios_1 = __importDefault(require("axios"));
-const url = 'https://jsonplaceholder.typicode.com/todos/1';
-const num = 1;
-const a = axios_1.default.get(url).then((r) => {
-    console.log(r.data);
-    console.log("<<NUMBER>>", num);
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+const PORT = process.env.port || 3000;
+const c = (a, b) => a + b;
+app.get('/', (req, res, next) => {
+    console.log(c(12, 55));
+    res.send('Hello world!');
+});
+app.listen(PORT, () => {
+    console.log(`Listing on port ${PORT}!`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,21 @@
-import axios from 'axios';
-const url: string = 'https://jsonplaceholder.typicode.com/todos/1'
-const num:Number = 1
-const a = axios.get(url).then((r) => {
-  console.log(r.data)
-  console.log("<<NUMBER>>", num)
+// import axios from 'axios';
+// const url: string = 'https://jsonplaceholder.typicode.com/todos/1'
+// const num:Number = 1
+// const a = axios.get(url).then((r) => {
+//   console.log(r.data)
+//   console.log("<<NUMBER>>", num)
+// })
+
+import express from 'express'
+
+const app = express();
+const PORT = process.env.port || 3000
+const c = (a: number, b: number): number => a+b
+app.get('/', (req, res, next) => {
+  console.log(c(12, 55));
+  res.send('Hello world!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listing on port ${PORT}!`)
 })
